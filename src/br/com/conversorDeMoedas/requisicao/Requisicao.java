88ca9c3eb42718aeb8b8520246cd0cd294649b9f
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-01-05 23:26:20 -0300

    desenvovido a requisicao

--- src/br/com/conversorDeMoedas/requisicao/Requisicao.java
+++ src/br/com/conversorDeMoedas/requisicao/Requisicao.java
@@ -1,25 +1,45 @@
 package br.com.conversorDeMoedas.requisicao;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Requisicao {
 
     private String moedaBase;
     private String moedaDestino;
-    private int cotacao;
-    String moedasUsadas = "";
-    String json;
-
+    private double cotacao;
+    private double quantidade;
+    private double cotacaoUsuario;
+    private String moedasUsadas;
+    private String json;
     Scanner entrada = new Scanner(System.in);
     HttpClient client = HttpClient.newHttpClient();
 
 
-    public void requisicao() throws IOException, InterruptedException {
+    public Requisicao(ApiNomes apiNomes) {
+        this.moedaBase = apiNomes.base_code();
+        this.moedaDestino = apiNomes.target_code();
+        this.cotacao = apiNomes.conversion_rate();
+    }
+
+    @Override
+    public String toString() {
+        return moedaBase + " É EQUIVALENTE A " + moedaDestino;
+    }
+
+
+
+    public Requisicao() {
+    }
+
+    public void Requisicao() throws IOException, InterruptedException {
 
         int opcao = 0;
 
@@ -38,42 +58,73 @@ public class Requisicao {
 
             System.out.println("*****************************************");
 
-            switch (opcao) {
-                case 1:
-                    moedasUsadas = "USD/ARS";
-                case 2:
-                    moedasUsadas = "ARS/USD";
-                case 3:
-                    moedasUsadas = "USD/BRL";
-                case 4:
-                    moedasUsadas= "BRL/USD";
-                case 5:
-                    moedasUsadas = "USD/EUR";
-                case 6:
-                    moedasUsadas = "EUR/USD";
-                case 7:
-                    System.out.println("Programa finalizado");
+
+            //Tentei utilizar o switch para deixar o código mais fácil de se entender mais não funcionou
+            // sempre um case atribuia o valor a variavel "moedasUsadas" independente da entrada do usuario
+//            switch (opcao) {
+//                case 1:
+//                    moedasUsadas = "USD/ARS";
+//                case 2:
+//                    moedasUsadas = "ARS/USD";
+//                case 3:
+//                    moedasUsadas = "USD/BRL";
+//                case 4:
+//                    moedasUsadas = "BRL/USD";
+//                case 5:
+//                    moedasUsadas = "USD/EUR";
+//                case 6:
+//                    moedasUsadas = "EUR/USD";
+//                case 7:
+//                    moedasUsadas = "";
+//                    System.out.println("Programa finalizado");
+//                default:
+//                    System.out.println("entrada invalida");
+//            }
+
+            if (opcao == 1) {
+                moedasUsadas = "USD/ARS";
             }
-            //Para o bloco while
+            if (opcao == 2) {
+                moedasUsadas = "ARS/USD";
+            }
+            if (opcao == 3) {
+                moedasUsadas = "USD/BRL";
+            }
+            if (opcao == 4) {
+                moedasUsadas = "BRL/USD";
+            }
+            if (opcao == 5) {
+                moedasUsadas = "USD/EUR";
+            }
+            if (opcao == 6) {
+                moedasUsadas = "EUR/USD";
+            }
+            //Para sair do bloco while
             if (opcao == 7) {
-                    break;
-                }
-
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://v6.exchangerate-api.com/v6/a68bd03a4e5ceb1c33cc089e/pair/" + moedasUsadas))
-                    .build();
-            try {
-                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-                json = response.body();
-
-            } catch (Exception e) {
-
+                moedasUsadas = "";
+                System.out.println("Programa finalizado");
+                break;
             }
+            if (opcao > 7 || opcao < 0) {
+                System.out.println("Digite uma opção válida");
+            } else {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("https://v6.exchangerate-api.com/v6/a68bd03a4e5ceb1c33cc089e/pair/" + moedasUsadas))
+                        .build();
+                try {
+                    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+                    json = response.body();
+                    System.out.println("Digite a quantia que deseja converter");
+                    quantidade = entrada.nextDouble();
+                    AcessaJson acessaJson = new AcessaJson(json, quantidade);
+                    cotacaoUsuario = acessaJson.conversao;
+                } catch (InputMismatchException e) {
+                    System.out.println("erro de digitação");
+                    System.out.println(e.getMessage());
+                }
+            }
+
         }
+
     }
-
-
-
-
-
 }
