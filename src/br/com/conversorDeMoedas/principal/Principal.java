777558752d86ebe9a796215a7ea1e2f371d42fa1
andrenevesxavier <andrenevesxavier276@gmail.com>
2026-01-05 23:27:56 -0300

    EXECUCAO PROGRAMA

--- src/br/com/conversorDeMoedas/principal/Principal.java
+++ src/br/com/conversorDeMoedas/principal/Principal.java
@@ -1,6 +1,13 @@
 package br.com.conversorDeMoedas.principal;
 
+import br.com.conversorDeMoedas.requisicao.AcessaJson;
+import br.com.conversorDeMoedas.requisicao.Requisicao;
+
+import java.io.IOException;
+
 public class Principal {
-    public static void main() {
+    public static void main() throws IOException, InterruptedException {
+        Requisicao requisicao = new Requisicao();
+        requisicao.Requisicao();
     }
 }
